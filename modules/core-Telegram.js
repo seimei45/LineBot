d9367a885290ab540aff7e8149b54461dda91b94
zeteticl <cougar.l@hotmail.com>
2019-05-31 18:33:41 +0800

    Update core-Telegram.js

--- modules/core-Telegram.js
+++ modules/core-Telegram.js
@@ -58,10 +58,9 @@ if (process.env.TELEGRAM_CHANNEL_SECRET) {
 				rplyVal = await exports.analytics.parseInput(ctx.message.text, groupid, userid, userrole)
 			} else {
 				if (channelKeyword == '') {
-					rplyVal = await exports.analytics.parseInput(ctx.message.text, groupid, userid, userrole).then(() => {
-						console.log('rplyVal: ', rplyVal)
+					rplyVal = await exports.analytics.parseInput(ctx.message.text, groupid, userid, userrole)
+					console.log('rplyVal: ', rplyVal)
 
-					})
 				}
 
 			}
