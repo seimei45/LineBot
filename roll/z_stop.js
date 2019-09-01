a8a4cd98b214997f0958a330cebe33080b0ca23e
zeteticl <cougar.l@hotmail.com>
2019-09-01 11:09:41 +0800

    down

--- roll/z_stop.js
+++ roll/z_stop.js
@@ -17,7 +17,7 @@ if (process.env.mongoURL) {
         return 'Block:hktrpg'
     }
     prefixs = function () {
-        return [/^[.]bk$/ig,]
+        return [/^[.]bk$/ig, ]
     }
     getHelpMessage = function () {
         return "【擲骰開關功能】" + "\
@@ -42,110 +42,10 @@ if (process.env.mongoURL) {
         rply.text = '';
         switch (true) {
 
-            case /^help$/i.test(mainMsg[1]):
-                rply.text = this.getHelpMessage();
-                return rply;
 
-            case /^add$/i.test(mainMsg[1]) && /^[\u4e00-\u9fa5a-zA-Z0-9]+$/ig.test(mainMsg[2]) && /^((?!^(b|k|bk)$).)*$/ig.test(mainMsg[2]):
-                //增加阻擋用關鍵字
-                if (groupid && mainMsg[2] && userrole >= 2) {
-                    let temp = {
-                        groupid: groupid,
-                        blockfunction: mainMsg[2]
-                    }
-                    records.pushblockfunction('block', temp, () => {
-                        records.get('block', (msgs) => {
-                            rply.save = msgs
-                        })
-
-                    })
-                    rply.text = '新增成功: ' + mainMsg[2]
-                } else {
-                    rply.text = '新增失敗.'
-                    if (!mainMsg[2])
-                        rply.text += '沒有關鍵字. '
-                    if (!groupid)
-                        rply.text += '不在群組. '
-                    if (groupid && userrole < 2)
-                        rply.text += '只有DM以上才可新增. '
-                }
-                return rply;
-
-            case /^del$/i.test(mainMsg[1]) && /^all$/i.test(mainMsg[2]):
-                //刪除阻擋用關鍵字
-                if (groupid && mainMsg[2] && rply.save && userrole >= 2) {
-                    for (var i = 0; i < rply.save.length; i++) {
-                        if (rply.save[i].groupid == groupid) {
-                            let temp = rply.save[i]
-                            temp.blockfunction = []
-                            records.set('block', temp, () => {
-                                records.get('block', (msgs) => {
-                                    rply.save = msgs
-                                })
-                            })
-                            rply.text = '刪除所有關鍵字'
-                        }
-                    }
-                } else {
-                    rply.text = '刪除失敗.'
-                    if (!groupid)
-                        rply.text += '不在群組. '
-                    if (groupid && userrole < 2)
-                        rply.text += '只有DM以上才可刪除. '
-                }
-
-                return rply;
-            case /^del$/i.test(mainMsg[1]) && /^\d+$/i.test(mainMsg[2]):
-                //刪除阻擋用關鍵字
-                if (groupid && mainMsg[2] && rply.save && userrole >= 2) {
-                    for (var i = 0; i < rply.save.length; i++) {
-                        if (rply.save[i].groupid == groupid && mainMsg[2] < rply.save[i].blockfunction.length && mainMsg[2] >= 0) {
-                            let temp = rply.save[i]
-                            temp.blockfunction.splice(mainMsg[2], 1)
-                            //console.log(rply.save[i])
-                            records.set('block', temp, () => {
-                                records.get('block', (msgs) => {
-                                    rply.save = msgs
-                                })
-                            })
-
-                        }
-                        rply.text = '刪除成功: ' + mainMsg[2]
-                    }
-                } else {
-                    rply.text = '刪除失敗.'
-                    if (!mainMsg[2])
-                        rply.text += '沒有關鍵字. '
-                    if (!groupid)
-                        rply.text += '不在群組. '
-                    if (groupid && userrole < 2)
-                        rply.text += '只有DM以上才可刪除. '
-                }
-                return rply;
-
-            case /^show$/i.test(mainMsg[1]):
-                records.get('block', (msgs) => {
-                    rply.save = msgs
-                })
-                if (groupid) {
-                    let temp = 0;
-                    for (var i = 0; i < rply.save.length; i++) {
-                        if (rply.save[i].groupid == groupid) {
-                            rply.text += '阻擋用關鍵字列表:'
-                            for (var a = 0; a < rply.save[i].blockfunction.length; a++) {
-                                temp = 1
-                                rply.text += ("\n") + a + '. ' + rply.save[i].blockfunction[a]
-                            }
-                        }
-                    }
-                    if (temp == 0) rply.text = '沒有阻擋用關鍵字. '
-                } else {
-                    rply.text = '不在群組. '
-                }
-                //顯示阻擋用關鍵字
-                return rply;
             default:
-                break;
+                rply.text = '因服務供應商出現問題，因此資料庫相關功能暫時停止服務.詳情請參考 https://status.mlab.com '
+                return rply;
         }
     }
 
